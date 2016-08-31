1c05e51356beb31c2410b4d853f198153fa1c99f
IzualVu <devilskullhp@gmail.com>
2016-08-31 20:36:08 +0700

    Create Main.php

---
+++ WindyInformation/Main.php
@@ -0,0 +1,14 @@
+<?php
+namespace WindyInformations;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+class Main extends PluginBase implements Listener {
+	
+	public function onEnable( ) {
+	        $this->getServer( )->getPluginManager( )->registerEvents($this, $this);
+	       	$this->saveDefaultConfig();
+                $this->getLogger( )->info(TextFormat::GREEN . "Windy Information Enabled - Izual");
+        }
